3eadd143dc8b71c6ead68ea049f188cdf43accb5
Gabriel-Picholari <gabriel.p.cunha@hotmail.com>
2025-07-28 23:05:23 -0300

    Resumed work on model training with a new algorithm: BDTs using gradient boosting. All code was adapted to meet the requirements of this approach. All macros included in this commit are currently functional, and the generated plots are most likely reliable.

--- W_boson_decay_project/modelTrainment_strange.cpp
+++ W_boson_decay_project/modelTrainment_strange.cpp
@@ -1,11 +1,3 @@
-//---------------------------------------------------------------------------------------------------------//
-//                                                                                                         //
-// Macro that utilizes the training and testing trees from both signal and background data to finally      //
-// train, test and evaluate the logistic regression method. Note that the library used is TMVA. The macro  //
-// also reads the results and print the confusion matrix.                                                  //
-//                                                                                                         //
-//---------------------------------------------------------------------------------------------------------//
-
 #include <TFile.h>
 #include <TTree.h>
 #include <TROOT.h>
@@ -16,7 +8,6 @@
 void modelTrainment_strange(const char *fileName) {
     
     TFile* outputFile = TFile::Open("TMVAOutput_2var_strange.root", "RECREATE");
-
     TFile* inputFile = TFile::Open(fileName, "READ");
 
     TTree* signalTree_s = dynamic_cast<TTree *>(inputFile->Get("SignalTree_s"));
@@ -25,38 +16,42 @@ void modelTrainment_strange(const char *fileName) {
     TMVA::Factory factory("TMVAClassification", outputFile, "AnalysisType=Classification");
     TMVA::DataLoader loader("dataset_s");
 
+    // Variáveis de entrada
     loader.AddVariable("pT_s", 'F');
     loader.AddVariable("nConst_s", 'F');
-    
-    
+
+    // Variáveis espectadoras
     loader.AddSpectator("eta_s", "F");
     loader.AddSpectator("phi_s", "F");
     loader.AddSpectator("mass_s", "F");
     loader.AddSpectator("label_s", "F");
     loader.AddSpectator("eventID_s", "F");
 
+    // Trees de sinal e fundo
     loader.AddSignalTree(signalTree_s, 1.0);
     loader.AddBackgroundTree(backgroundTree_s, 1.0);
 
     //---------------------------------------------------------------------------------------------------------
-    // Divisao das Trees de sinal e fundo em treinamento e teste
+    // Divisão manual das Trees de sinal e fundo em treinamento e teste
     //---------------------------------------------------------------------------------------------------------
 
     TCut mycut = "";
+
     Int_t nTest_Signal = signalTree_s->GetEntries() * 0.2;
     Int_t nTrain_Signal = signalTree_s->GetEntries() * 0.8;
 
     Int_t nTest_Background = backgroundTree_s->GetEntries() * 0.2;
     Int_t nTrain_Background = backgroundTree_s->GetEntries() * 0.8;
-    
-    TString options = TString::Format("SplitMode=Random:SplitSeed=0:NormMode=None:nTrain_Signal=%d:nTrain_Background=%d:nTest_Signal=%d:nTest_Background=%d:!V", nTrain_Signal, nTrain_Background, nTest_Signal, nTest_Background);
+
+    TString options = TString::Format("SplitMode=Random:SplitSeed=0:NormMode=None:nTrain_Signal=%d:nTrain_Background=%d:nTest_Signal=%d:nTest_Background=%d:", nTrain_Signal, nTrain_Background, nTest_Signal, nTest_Background);
+
     loader.PrepareTrainingAndTestTree(mycut, options.Data());
 
     //---------------------------------------------------------------------------------------------------------
-    // Escolha do metodo de treinamento e concretizacao de teste, treino e evaluacao do modelo
+    // Treinamento com GradBoost (mesma configuração do charm)
     //---------------------------------------------------------------------------------------------------------
 
-    factory.BookMethod(&loader, TMVA::Types::kLikelihood, "Likelihood", "H:!V");
+    factory.BookMethod(&loader, TMVA::Types::kBDT, "GradBoost", "BoostType=Grad:Shrinkage=0.1:MaxDepth=3");
 
     factory.TrainAllMethods();
     factory.TestAllMethods();
@@ -64,4 +59,4 @@ void modelTrainment_strange(const char *fileName) {
 
     inputFile->Close();
     outputFile->Close();    
-    }
+}
